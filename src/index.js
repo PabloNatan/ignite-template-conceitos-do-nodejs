dcfe6d88b37f7bd2b8994ecd272b05d02079bb7e
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2022-05-09 05:34:31 -0300

    feat: route to create a user

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
@@ -16,10 +16,44 @@ function checksExistsUserAccount(request, response, next) {
 
 app.post('/users', (request, response) => {
   // Complete aqui
+  const { username, name } = request.body;
+
+  const newUser = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: [],
+  };
+
+  users.push(newUser);
+
+  response.json(newUser);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+
+  const { title, deadline, username } = request.body;
+
+  const newTodo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date(),
+  };
+
+  const updatedUsers = users.map((user) => {
+    if (user.username === username) {
+      return user.todos.push(newTodo);
+    }
+    return user;
+  });
+
+  const users = updatedUsers;
+  console.log(users);
+
+  response.json(newTodo);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
